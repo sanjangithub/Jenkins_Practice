297142d2061829057699f997504146d3e291e47d
sanjangithub <sanjan.chikkala@gmail.com>
2024-11-30 18:11:26 +0530

    Used few stream api methods in the main method..

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello
 {
     public static void main(String[] args) {
         System.out.println("My self Sanjan.");
+        List<String> names = new ArrayList<>();
+        names.add("Sanjan"); names.add("Jaikar"); names.add("Harshini"); names.add("Bharathi");
+
+        names.stream().map(i -> i.equals("Bharathi") ? "Karra" : i + " Chikkala").forEach(System.out::println);
     }
 }
