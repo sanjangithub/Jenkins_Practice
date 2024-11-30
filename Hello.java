0902fbe9ec1b9559d912db2cfcc1395822cbf04b
sanjangithub <sanjan.chikkala@gmail.com>
2024-11-30 11:44:52 +0530

    Small change in the output

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello
 {
     public static void main(String[] args) {
-        System.out.println("Hello from java hello class.");
+        System.out.println("My self Sanjan.");
     }
 }
